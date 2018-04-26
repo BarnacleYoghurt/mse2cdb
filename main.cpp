f5f45f8bd3464ec686d344ceee6231273b4f6f13
utso <cccygodev@gmail.com>
2018-04-26 23:58:01 +0200

    Maybe it will work from bash.

--- main.cpp
+++ main.cpp
@@ -36,6 +36,5 @@ int main(int argc, char **argv) {
         std::cout << "Oh, but I was totally called for language " << argLanguage << " and template " << argTemplate
                   << "." << std::endl;
     }
-
     return 0;
 }
