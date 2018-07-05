f2d1d332995439bd130408645f8cc7ae1432eeb6
utso <cccygodev@gmail.com>
2018-07-05 15:21:02 +0200

    Add CMake command to copy lua scripts into build directory and change LuaScriptFinder to get them from there.

--- io/LuaScriptFinder.cpp
+++ io/LuaScriptFinder.cpp
@@ -3,19 +3,19 @@
 namespace io{
     namespace luaScriptFinder{
         std::string getConstantsPath(){
-            return "../lua/constant.lua";
+            return "./lua/constant.lua";
         }
         std::string getUtilityPath(){
-            return "../lua/utility.lua";
+            return "./lua/utility.lua";
         }
         std::string getTemplatePath(const std::string &templateName){
-            return "../lua/" + templateName + ".lua";
+            return "./lua/" + templateName + ".lua";
         }
         std::string getDictPath(const std::string &language){
-            return "../lua/lang/" + language + "/dict.lua";
+            return "./lua/lang/" + language + "/dict.lua";
         }
         std::string getArchetypesPath(const std::string &language){
-            return "../lua/lang/" + language + "/archetypes.lua";
+            return "./lua/lang/" + language + "/archetypes.lua";
         }
     }
 }
